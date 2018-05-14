050a1868cb1ea0024eb5c11d9eb54c1ad75f4f03
mhsander <marthahsander@gmail.com>
2018-05-14 10:40:13 -0700

    model and view for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,28 @@
 #Controller for page where guests can leave a comment to the grooms
 
 class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
   def new
 
   end
 
   def create
-    render plain: params[:comment].inspect
+    @comment = Comment.new(comment_params)
+    @comment.save
+    redirect_to @comment
   end
 
-  def show
+  private
+  # defines the allowed parameters for comments
 
+  def comment_params
+    params.require(:comment).permit(:name, :text)
   end
-
 end
