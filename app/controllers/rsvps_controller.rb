2d72c518e2ef8aba5a2ca37267ee8d5eb3becae8
mhsander <marthahsander@gmail.com>
2018-05-21 14:31:06 -0700

    rsvp completed

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,6 +1,8 @@
 #controller for RSVP form
 
 class RsvpsController < ApplicationController
+  http_basic_authenticate_with name: 'IzzySid', password: 'wedding', only: [:destroy]
+
   def index
   @rsvps = Rsvp.all
   end
@@ -23,25 +25,32 @@ class RsvpsController < ApplicationController
     if @rsvp.save
       redirect_to @rsvp
     else
-      redirect_to '/rsvps'
+      render 'new'
     end
   end
 
-  # def update
-  #   @rsvp = Rsvp.find(params[:id])
-  #
-  #   if @rsvp.update(rsvp_params)
-  #     redirect_to @rsvp
-  #   else
-  #     render 'index'
-  #   end
-  # end
+  def update
+    @rsvp = Rsvp.find(params[:id])
+
+    if @rsvp.update(rsvp_params)
+      redirect_to @rsvp
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @rsvp = Rsvp.find(params[:id])
+    @rsvp.destroy
+    redirect_to @rsvp
+  end
 
   private
 
   def rsvp_params
-  params.permit(:name, :address, :phone, :email, :coming,
-                :plus_one_name, :mini_you, :name_and_age, :food, :allergies, :terms)
+    params.require(:rsvp).permit(:name, :address, :phone, :email, :coming,
+                                 :plus_one_name, :mini_you, :name_and_age,
+                                 :food, :allergies, :terms)
   end
 
 end
